d586690c3d9a2b4bcaac609de43ed2dcb7c88a97
everydayduffy <james.philip.duffy@gmail.com>
2018-04-25 11:43:03 +0100

    functionsed log formatting

--- mapping/temp_geojson_creator.py
+++ mapping/temp_geojson_creator.py
@@ -9,9 +9,12 @@ import csv
 #variables#
 ###########
 
-temp_file = "C:/Sandbox/geojson/college_valley_1_temperature.log"
-gps_file = "C:/Sandbox/geojson/college_valley_1.log"
-out_file = "C:/Sandbox/geojson/test3.geojson"
+log_path = "C:/Sandbox/geojson"
+temp_log_name = "college_valley_1_temperature.log"
+gps_log_name = "college_valley_1.log"
+
+out_path = "C:/Sandbox/geojson"
+out_file = "temp_test1.geojson"
 
 ############
 #functions#
@@ -25,9 +28,44 @@ def read_log(file):
             the_list.append(row)
     return(the_list)
 
-def remove_values_from_list(the_list, val):
-   return [value for value in the_list if value != val]
-   
+#def remove_values_from_list(the_list, val):
+#   return [value for value in the_list if value != val]
+
+def format_temp_log(file):
+    log_list = read_log(file)
+    log_list = [k for k in log_list if "New session started..." not in k]
+    #remove empty lines
+    log_list = list(filter(None, log_list))
+    log_df = pd.DataFrame(log_list, columns = ["date", "time", "id", "temp"])
+    #calculate a mean temperature for repeat measures on a given time stamp
+    log_df[['temp']] = log_df[['temp']].apply(pd.to_numeric)
+    log_df = log_df.groupby(['date','time'], as_index=False).mean()
+    return(log_df)
+    
+def format_gps_log(file):
+    log_list = read_log(file)
+    log_list = [k for k in log_list if "New session started..." not in k and
+                                   "No data feed from driver" not in k and
+                                   "No driver found" not in k and
+                                   "Now using GPS date and time" not in k]
+    log_df = pd.DataFrame(log_list, columns = ["date", "time", "lat", "lon", 
+                                           "alt", "num_sats", "hdop", "qi", 
+                                           "code"])
+    #filter out fixes
+    log_df = log_df[log_df['code'] == "FIX"]
+    #select only wanted columns
+    log_df = log_df[["date","time","lat","lon"]]
+    return(log_df)
+    
+def join_temp_gps(temp_df, gps_df):
+    #join data frames
+    j_data = pd.merge(temp_df, gps_df, on=['date','time'], how='inner')
+    #ensure numeric columns are numeric!
+    j_data[['lat','lon']] = j_data[['lat','lon']].apply(pd.to_numeric)
+    #drop duplicate times
+    j_data = j_data.drop_duplicates("time")
+    return(j_data)
+
 def df_to_geojson(df,out_file):
     features = []
     insert_features = lambda X: features.append(
@@ -44,44 +82,10 @@ def df_to_geojson(df,out_file):
 #running code#
 ##############
 
-#temperature data
-temp_list = read_log(temp_file)
-#filter
-temp_list = [k for k in temp_list if "New session started..." not in k]
-#remove empty lines
-temp_list = list(filter(None, temp_list))
-temp_df = pd.DataFrame(temp_list, columns = ["date", "time", "id", "temp"])
-#calculate a mean temperature for repeat measures on a given time stamp
-temp_df[['temp']] = temp_df[['temp']].apply(pd.to_numeric)
-temp_df = temp_df.groupby(['date','time'], as_index=False).mean()
+temp_df = format_temp_log(log_path + "/" + temp_log_name)
 
-#GPS data
-gps_list = read_log(gps_file)
-#filter
-gps_list = [k for k in gps_list if "New session started..." not in k and
-                                   "No data feed from driver" not in k and
-                                   "No driver found" not in k and
-                                   "Now using GPS date and time" not in k]
-gps_df = pd.DataFrame(gps_list, columns = ["date", "time", "lat", "lon", 
-                                           "alt", "num_sats", "hdop", "qi", 
-                                           "code"])
-#filter out fixes
-gps_df = gps_df[gps_df['code'] == "FIX"]
-#select only wanted columns
-gps_df = gps_df[["date","time","lat","lon"]]
+gps_df = format_gps_log(log_path + "/" + gps_log_name)
 
+j_data = join_temp_gps(temp_df, gps_df)
 
-#join data frames
-j_data = pd.merge(temp_df, gps_df, on=['date','time'], how='inner')
-#ensure numeric columns are numeric!
-j_data[['lat','lon']] = j_data[['lat','lon']].apply(pd.to_numeric)
-#drop duplicate times
-j_data = j_data.drop_duplicates("time")
-
-
-df_to_geojson(j_data,out_file)
-
-
-
-
-
+df_to_geojson(j_data, out_path + "/" + out_file)
